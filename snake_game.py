bdc731d242970e7eb9bcde4b8b7fc9cea6b561e1
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-12-17 22:53:39 -0800

    moving snake

--- snake_game.py
+++ snake_game.py
@@ -6,14 +6,22 @@ import pygame
 def snakegame():
     pygame.init()
 
+    white = (255, 255, 255) # screen
+    black=(0,0,0)          # snake
+    red=(255,0,0)           # blocks
+
+    x1 = 300
+    y1 = 300
+    x1_change = 0
+    y1_change = 0
+
+    clock = pygame.time.Clock()
+
     # game window
-    dis = pygame.display.set_mode((400, 300))
+    dis = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Snake Game by Griffin")
 
-    pygame.display.update()
-
-    blue=(0,0,255)
-    red=(255,0,0)
+    # pygame.display.update()
 
     # prev auto quit
     game_over = False
@@ -22,8 +30,30 @@ def snakegame():
             # close on x
             if event.type == pygame.QUIT: 
                 game_over = True
-        pygame.draw.rect(dis, blue, [200,150,10,10])
+            # move snake
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -10
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = 10
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    x1_change = 0
+                    y1_change = -10
+                elif event.key == pygame.K_DOWN:
+                    x1_change = 0
+                    y1_change = 10
+        x1 += x1_change
+        y1 += y1_change
+        
+        dis.fill(white)
+        pygame.draw.rect(dis, black, [x1, y1, 10, 10]) # snake
+        
         pygame.display.update()
+
+        clock.tick(30)
+
 
 
     # exit
@@ -33,23 +63,3 @@ def snakegame():
 
 if __name__ == "__main__":
     snakegame()
-
-
-# import pygame
-# pygame.init()
-# dis=pygame.display.set_mode((400,300))
- 
-# pygame.display.set_caption('Snake game by Edureka')
- 
-# blue=(0,0,255)
-# red=(255,0,0)
- 
-# game_over=False
-# while not game_over:
-#     for event in pygame.event.get():
-#         if event.type==pygame.QUIT:
-#             game_over=True
-#     pygame.draw.rect(dis,blue,[200,150,10,10])
-#     pygame.display.update()
-# pygame.quit()
-# quit()
